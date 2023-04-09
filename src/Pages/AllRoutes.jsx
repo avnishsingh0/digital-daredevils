0366e38a631518e203da8e875fc593dc077fcd4e
Soaib Akhtar <98397413+soaibakhtar@users.noreply.github.com>
2023-04-09 20:53:54 +0530

    Leaderboard

--- src/Pages/AllRoutes.jsx
+++ src/Pages/AllRoutes.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Homepage from './LandingPage'
 import Canvas from "../Components/Canvas";
+import { Leaderboard } from "../Components/Leaderboard";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/game" element={<Canvas />} />
+      <Route path="/leaderboard" element={<Leaderboard/>} />
     </Routes>
   );
 };
